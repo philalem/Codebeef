ba14f3c75ea280f932773b6303943a7e9f64d584
Phillip LeMaster <philalem21@gmail.com>
2020-08-03 19:05:50 -0400

    Initial Commit

---
+++ Codebeef/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Codebeef
+//
+//  Created by Phillip LeMaster on 8/3/20.
+//  Copyright © 2020 Phillip LeMaster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CodebeefTests/CodebeefTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodebeefTests.swift
+//  CodebeefTests
+//
+//  Created by Phillip LeMaster on 8/3/20.
+//  Copyright © 2020 Phillip LeMaster. All rights reserved.
+//
+
+import XCTest
+@testable import Codebeef
+
+class CodebeefTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
